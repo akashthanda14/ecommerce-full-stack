0f35c956d95f164a8819b84ec21741ab4ebe833a
work <akashthanda14@gmail.com>
2025-06-29 20:06:58 +0530

    added to the branch

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import {
   placeOrder,
-  createStripeSession,
+  // createStripeSession,
   // createRazorpayOrder,
   // verifyRazorpayPayment,
   getAllOrderList,
