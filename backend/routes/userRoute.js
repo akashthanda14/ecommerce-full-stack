bd3bd7eba57ee019abb108c74043db00bfa4597b
work <akashthanda14@gmail.com>
2025-07-03 11:41:35 +0530

    Rate-limit helmet implememnted

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,10 +2,11 @@
 import express from 'express';
 import { loginUser, registerUser } from '../controllers/userController.js';
 import { validateRegister, validateLogin } from '../validators/userValidator.js';
+import { authLimiter } from '../middleware/rateLimit.js';
 
 const router = express.Router();
 
-router.post('/login', validateLogin, loginUser);
-router.post('/register', validateRegister, registerUser);
+router.post('/login',validateLogin, loginUser);
+router.post('/register', authLimiter,validateRegister, registerUser);
 
 export default router;
